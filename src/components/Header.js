625896f6a89613f8beca9ffb3232937d69df9352
Abhinav Dixit <abhinavdixit@192.168.1.5>
2024-04-05 17:55:02 +0530

    signout userSlice, update profile, signin api

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { ReactComponent as Logo } from "../assets/images/logo.svg";
+import { signOut } from 'firebase/auth';
+import { auth } from '../utils/firebase';
+import { useNavigate } from 'react-router-dom';
+import { UseSelector, useSelector } from 'react-redux';
 
 
 const Header = () => {
-  return (
-    <div className='absolute  m-10  bg-gradient-to-b from-black z-10'>
-         <Logo className='w-44'/>
-    </div>
-   
-  )
-}
+  const navigate = useNavigate();
+  const user = useSelector((store)=>store.user);
+  const [showDropdown, setShowDropdown] = useState(false);
 
-export default Header
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  const handleLogout = () => {
+    signOut(auth).then(() => {
+      navigate("/");
+    }).catch((error) => {
+    });
+  };
+
+  return (
+    <div className='absolute top-0 left-0 right-0 bg-gradient-to-b from-black z-10 flex justify-between items-center py-4 px-6'>
+      <Logo className='w-44' />
+      {user &&  <div className="relative">
+        <button className="flex items-center" onClick={toggleDropdown}>
+          <img src={user?.photoURL||"https://occ-0-4409-3647.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABZD5V1r90jfejoixi1jJ1EkPvN6SDGZygT418_r3e6h5oFrh5YbwAZLnfyPB-dSX7j72qV7PYnW4vmb9hUwPQJR2AqfnUUk.png?r=805"} alt="Logout" className="h-6 w-6 mr-2" />
+          <span className="text-white"></span>
+        </button>
+        {showDropdown && (
+          <div className="absolute right-0 mt-2 bg-red-700 rounded-md shadow-lg w-auto">
+            <button
+              className="block w-full text-left px-4 py-2 text-sm text-gray-300 hover:rounded-md hover:bg-red-800"
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+          </div>
+        )}
+      </div>
+}
+    </div>
+  );
+};
+
+export default Header;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,14 @@
 import React, { useState, useRef } from 'react';
 import Header from './Header';
 import { checkValidateData } from '../utils/validate';
+import { useNavigate } from 'react-router-dom';
 import { BANNER_URL } from '../utils/constants';
-import { createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword,signInWithEmailAndPassword,updateProfile } from "firebase/auth";
 import { auth } from '../utils/firebase'
 import sound from '../assets/sounds/themesong.mp3'
 import useSound from 'use-sound';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
 
 
 
@@ -14,9 +17,12 @@ const Login = () => {
     const [isSigninForm, setIsSigninForm] = useState(true);
     const [showPassword, setShowPassword] = useState(false);
     const [errorMessage, setErrorMessage] = useState(null);
+    const dispatch = useDispatch();
     const email = useRef(null);
     const password = useRef(null);
+    const name = useRef(null);
     const [play] = useSound(sound);
+    const navigate = useNavigate();
 
 
     const toggleSignIn = () => {
@@ -34,13 +40,26 @@ const Login = () => {
                 .then((userCredential) => {
                     // Signed up 
                     const user = userCredential.user;
+                    updateProfile(user, {
+                        displayName: name.current.value, photoURL: "https://avatars.githubusercontent.com/u/56727751?v=4"
+                      }).then(() => {
+                        const {uid, email, displayName, photoURL} = auth.currentUser;
+                        dispatch(addUser({uid:uid, email:email, displayName:displayName, photoURL: photoURL}));
+                        navigate("/browse");
+                      }).catch((error) => {
+                       setErrorMessage(error.message);
+                       
+                      });
+                      
                     console.log(user);
+                   
   })
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
     setErrorMessage(errorCode + "-" + errorMessage)
     play();
+    
   });
 
         }
@@ -50,6 +69,7 @@ const Login = () => {
                     // Signed in 
                     const user = userCredential.user;
                     console.log(user);
+                    navigate("/browse");
                 })
                 .catch((error) => {
                     const errorCode = error.code;
@@ -73,7 +93,7 @@ const Login = () => {
             </div>
             <form onSubmit={(e)=> e.preventDefault()} className='absolute p-12 bg-black w-1/4 h-2/3 my-auto top-0 bottom-0 mx-auto right-0 left-0 bg-opacity-80 rounded-md text-white'>
                 <h1 className='text-3xl font-bold py-4'>{isSigninForm ? "Sign In" : "Sign up"}</h1>
-                {!isSigninForm && <input type='text' placeholder='Name' className='p-4 my-4 w-full bg-inherit border border-gray-700 rounded-md bg-opacity-70 hover:border-red-700'/>}
+                {!isSigninForm && <input type='text' ref={name} placeholder='Name' className='p-4 my-4 w-full bg-inherit border border-gray-700 rounded-md bg-opacity-70 hover:border-red-700'/>}
                 <input ref={email} type='text' placeholder='Email Address' className='p-4 my-4 w-full bg-inherit border border-gray-700 rounded-md bg-opacity-70 hover:border-red-700'/>
                 <div className="relative">
                    
